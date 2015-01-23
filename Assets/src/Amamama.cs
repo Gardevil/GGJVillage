6e43a6e1cb2e2ffe6dd4bc37e0363efb913fbf37
Miguel <miguelgardev@gmail.com>
2015-01-24 00:15:23 +0100

    prueba merge

--- Assets/src/Amamama.cs
+++ Assets/src/Amamama.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class Amamama : MonoBehaviour {
 
+    public string a;
+
 	// Use this for initialization
 	void Start () {
-	
+	//base.GetComponent<>
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //transform.position.x = 0;
+        }
 	}
 }
