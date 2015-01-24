877368cefb5be53d94d07383cf9116dda2eca019
Turri <carlos.turri.m@gmail.com>
2015-01-24 18:16:06 +0100

    Villager y movement

--- Assets/src/Villager.cs
+++ Assets/src/Villager.cs
@@ -5,14 +5,52 @@ public class Villager : MonoBehaviour {
 
     bool onGodDuty;/// Indica si está haciendo algo porque Dios lo ha dicho o por cuenta propia
 
+	Movement movement;
+
+
+	public int lifes{
+		get{
+			return lifes;
+		}
+
+	}
+
+	public int food{
+		get{
+			return food;
+		}
+	}
+	public int stamina{
+		get{
+			return stamina;
+		}
+	}
+
+
+
+
+	private int mLifes;
+	private int mFood;
+	private int mStamina;
+
+	public bool standing{
+		get{
+			return movement.standing();
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
         onGodDuty = false;
+		movement = GetComponent<Movement> ();
+		lifes = 5;
+		food = 5;
+		stamina = 5;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
     internal void SetBusy(bool onGodDuty)
@@ -20,8 +58,20 @@ public class Villager : MonoBehaviour {
         this.onGodDuty = onGodDuty;
     }
 
-    public bool isOnGodDuty()
-    {
-        return onGodDuty;
-    }
+	public void moveTo(Vector3 target){
+		movement.MoveTo(target);
+	}
+
+	public void moveToWareHouse(){
+		movement.MoveTo (FindManager.getWareHouse ());
+	}
+
+	public void moveToTotem(){
+		movement.MoveTo (FindManager.getTotem ());
+	}
+
+
+
+
+	
 }
